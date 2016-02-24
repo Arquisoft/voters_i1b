4bfc0d70d9d76a1bef6476eccdcfc582e0374f25
Dani <danicuestasuarez@gmail.com>
2016-02-25 00:00:43 +0100

    Fixed test

--- src/test/java/asw/voterAccess/MainControllerTest.java
+++ src/test/java/asw/voterAccess/MainControllerTest.java
@@ -42,58 +42,58 @@ public class MainControllerTest {
 		String userURI = base.toString();  
 		ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
 		assertThat(response.getBody(), equalTo("<!DOCTYPE html>"+
-				"<html lang=\"en\">"+
-				"	<head>"+
-				"		<meta charset=\"UTF-8\">"+
-				"		<title>Voting System</title>"+
-				"		<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js\"></script>		"+
-				"	</head>"+
-				"	<body>"+
-				"		<form id=form>"+
-				"			<label for=\"_user_email\">Email:</label>"+
-				"			<input id= \"_user_email\" type=\"text\" name=\"email\"><br>"+
-				"			<label for=\"_user_pass\">Password:</label>"+
-				"			<input id=\"_user_pass\" type=\"password\" name=\"password\"><br> "+
-				"			<input type=\"button\" value=\"Submit\" onclick=\"submitform()\" />"+
-				"		</form>"+
-				"		<div id=code></div>"+
-				"		"+
-				"		"+
-				"		<script type=\"text/javascript\">	"+
-				"		"+
-				"		$.fn.serializeObject = function()"+
-				"		{"+
-				"		   var o = {};"+
-				"		   var a = this.serializeArray();"+
-				"		   $.each(a, function() {"+
-				"		       if (o[this.name]) {"+
-				"		           if (!o[this.name].push) {"+
-				"		               o[this.name] = [o[this.name]];"+
-				"		           }"+
-				"		           o[this.name].push(this.value || '');"+
-				"		       } else {"+
-				"		           o[this.name] = this.value || '';"+
-				"		       }"+
-				"		   });"+
-				"		   return o;"+
-				"		};"+
-				"					"+
-				"    		function submitform(){"+
-				"    			var frm = $(\"form\");"+
-				"    			var data = JSON.stringify(frm.serializeObject());"+
-				"        		var xhttp = new XMLHttpRequest();"+
-				"        		xhttp.onreadystatechange = function() {"+
-				"        		    if (xhttp.readyState == 4 && xhttp.status == 200) {"+
-				"        		      document.getElementById(\"code\").innerHTML = xhttp.responseText;"+
-				"        		    }"+
-				"        		  };"+
-				"        		xhttp.open(\"POST\", \"user\", true);"+
-				"        		xhttp.setRequestHeader(\"Content-Type\", \"application/json\");"+
-				"        		xhttp.send(data);"+
-				"        		}"+
-				"        </script>"+
-				"	</body>"+
-				"</html>"));
+				"<!DOCTYPE html>"+
+"<html lang=\"en\">"+
+"	<head>"+
+"		<link rel=\"stylesheet\" href=\"http://yui.yahooapis.com/pure/0.6.0/pure-min.css\">"+
+"		<meta charset=\"UTF-8\">"+
+"		<title>Voting System</title>"+
+"		<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js\"></script>		"+
+"	</head>"+
+"	<body></br>"+
+"		<form id=form class=\"pure-form\">"+
+"			<input id= \"_user_email\" type=\"text\" name=\"email\"  placeholder=\"Email\">"+
+"			<input id=\"_user_pass\" type=\"password\" name=\"password\" placeholder=\"Password\">"+
+"			<input type=\"button\" value=\"Submit\" onclick=\"submitform()\" class=\"pure-button pure-button-primary\"/>"+
+"		</form>"+
+"		<div id=code></div>"+
+"		"+
+"		"+
+"		<script type=\"text/javascript\">	"+
+"		"+
+"		$.fn.serializeObject = function()"+
+"		{"+
+"		   var o = {};"+
+"		   var a = this.serializeArray();"+
+"		   $.each(a, function() {"+
+"		       if (o[this.name]) {"+
+"		           if (!o[this.name].push) {"+
+"		               o[this.name] = [o[this.name]];"+
+"		           }"+
+"		           o[this.name].push(this.value || '');"+
+"		       } else {"+
+"		           o[this.name] = this.value || '';"+
+"		       }"+
+"		   });"+
+"		   return o;"+
+"		};"+
+"					"+
+"    		function submitform(){"+
+"    			var frm = $(\"form\");"+
+"    			var data = JSON.stringify(frm.serializeObject());"+
+"        		var xhttp = new XMLHttpRequest();"+
+"        		xhttp.onreadystatechange = function() {"+
+"        		    if (xhttp.readyState == 4 && xhttp.status == 200) {"+
+"        		      document.getElementById(\"code\").innerHTML = xhttp.responseText;"+
+"        		    }"+
+"        		  };"+
+"        		xhttp.open(\"POST\", \"user\", true);"+
+"        		xhttp.setRequestHeader(\"Content-Type\", \"application/json\");"+
+"        		xhttp.send(data);"+
+"        		}"+
+"        </script>"+
+"	</body>"+
+"</html>"));
 	}
 	
 	@Test
